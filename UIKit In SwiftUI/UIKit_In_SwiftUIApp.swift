676e8efdca651d6f2941b755464027a14c7a456d
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-05 15:49:43 -0600

    Initial Commit

---
+++ UIKit In SwiftUI/UIKit_In_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_In_SwiftUIApp.swift
+//  UIKit In SwiftUI
+//
+//  Created by Fred Javalera on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_In_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
